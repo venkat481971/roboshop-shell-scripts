d048a581c052fe4faaa8423ca7e99d01d79b6051
venkat <venkymec1922@gmail.com>
2022-09-16 12:33:11 +0530

    conditions

--- catalogue.sh
+++ catalogue.sh
@@ -2,41 +2,73 @@ LOG_FILE=/tmp/catalogue
 
 echo 'setup nodejs repos'
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCES
+else
+  echo status = FAILURE
+fi
 
 echo 'install nodejs'
 yum install nodejs -y &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCES
+else
+  echo status = FAILURE
+fi
 
-echo 'add roboshop user'
+echo 'Add Roboshop Application user'
 useradd roboshop &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCES
+else
+  echo status = FAILURE
+fi
 
 echo 'download catalogue application code'
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCES
+else
+  echo status = FAILURE
+fi
 
 $ cd /home/roboshop
 
 echo 'extraction catalogue app code'
-$ unzip /tmp/catalogue.zip &>>${LOG_FILE}
-echo status = $?
+unzip /tmp/catalogue.zip &>>${LOG_FILE}
+if [ $? -eq 0 ]; then
+  echo status = SUCCES
+else
+  echo status = FAILURE
+fi
 
-$ mv catalogue-main catalogue
-$ cd /home/roboshop/catalogue
+mv catalogue-main catalogue
+cd /home/roboshop/catalogue
 
 echo 'install nodejs  dependencies'
-$ npm install
-echo status = $?
+npm install &>>${LOG_FILE}
+if [ $? -eq 0 ]; then
+  echo status = SUCCES
+else
+  echo status = FAILURE
+fi
 
 echo 'setup catalogue service'
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCES
+else
+  echo status = FAILURE
+fi
 
 
 systemctl daemon-reload &>>${LOG_FILE}
 systemctl start catalogue &>>${LOG_FILE}
 
 echo 'start catalogue service'
-systemctl enable catalogue
-echo status = $?
+systemctl enable catalogue &>>${LOG_FILE}
+if [ $? -eq 0 ]; then
+  echo status = SUCCES
+else
+  echo status = FAILURE
+fi
