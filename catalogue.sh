a4dc212c9d561ec3455cfb9f5dd6154fddd207ea
venkat <venkymec1922@gmail.com>
2022-09-17 13:18:38 +0530

    user

--- catalogue.sh
+++ catalogue.sh
@@ -1,52 +1,6 @@
 LOG_FILE=/tmp/catalogue
 
 source common.sh
+COMPONENT=catalogue
 
-echo 'setup nodejs repos'
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-StatusCheck $?
-
-
-echo 'install nodejs'
-yum install nodejs -y &>>${LOG_FILE}
-StatusCheck $?
-
-id roboshop &>>${LOG_FILE}
-if [ $? -ne 0 ]; then
- echo 'Add Roboshop Application user'
- useradd roboshop &>>${LOG_FILE}
- StatusCheck $?
-fi
-
-echo 'download catalogue application code'
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-StatusCheck $?
-
-cd /home/roboshop
-
-echo "clean old app content"
-rm -rf catalogue &>>${LOG_FILE}
-StatusCheck $?
-
-echo "Extract Catalogue Application Code"
-unzip /tmp/catalogue.zip &>>${LOG_FILE}
-StatusCheck $?
-
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
-
-echo "Install NodeJS Dependencies"
-npm install &>>${LOG_FILE}
-StatusCheck $?
-
-echo 'setup catalogue service'
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-StatusCheck $?
-
-
-systemctl daemon-reload &>>${LOG_FILE}
-systemctl start catalogue &>>${LOG_FILE}
-
-echo 'start catalogue service'
-systemctl enable catalogue &>>${LOG_FILE}
-StatusCheck $?
+NODEJS
