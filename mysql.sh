e97ad3231e87e018484c708a7839180b44709d87
venkat <venkymec1922@gmail.com>
2022-09-19 12:50:38 +0530

    sql

--- mysql.sh
+++ mysql.sh
@@ -21,33 +21,6 @@ StatusCheck $?
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$ROBOSHOP_MYSQL_PASSWORD}');
-FLUSH PRIVILEGES;" >/tmp/root-pass.sql
+FLUSH PRIVILEGES;"
 
-echo "show database;" |mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-echo "change the default root password"
-mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
-StatusCheck $?
-fi
 
-echo 'show plugins'| mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} 2>/dev/null | grep validate_password &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-echo "uninstall password validation plugin"
-echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
-StatusCheck $?
-fi
-
-echo "Download schema"
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
-StatusCheck $?
-
-echo "Extract schema"
-cd /tmp
-unzip -o mysql.zip &>>$LOG_FILE
-StatusCheck $?
-
-echo "load schema"
-cd mysql-main
-mysql -u root -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>$LOG_FILE
-StatusCheck $?
-fi
