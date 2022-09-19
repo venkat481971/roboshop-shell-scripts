58284cdc8f52713babaa3101d8d465561e5b7274
venkat <venkymec1922@gmail.com>
2022-09-19 16:53:53 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -28,6 +28,7 @@ if [ $? -ne 0 ]; then
 echo "change the default root password"
 mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
 StatusCheck $?
+fi
 
 
 
