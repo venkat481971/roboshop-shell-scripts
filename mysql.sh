df2c7142a428e0aae7a7f71414a77ac09105c63b
venkat <venkymec1922@gmail.com>
2022-09-19 12:53:08 +0530

    sql

--- mysql.sh
+++ mysql.sh
@@ -21,6 +21,6 @@ StatusCheck $?
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$ROBOSHOP_MYSQL_PASSWORD}');
-FLUSH PRIVILEGES;"
+FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
 
