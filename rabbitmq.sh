a4de98e3c87516ae9995c7d735cdeed83a7d5a4d
venkat <venkymec1922@gmail.com>
2022-09-20 13:43:03 +0530

    rabbit

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -7,7 +7,7 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 StatusCheck $?
 
 echo "install Erland & Rabbitmq"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG_FILE
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>$LOG_FILE
 StatusCheck $?
 
 echo "start RabbitMQ server"
