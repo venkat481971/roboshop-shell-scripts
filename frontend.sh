c55787cd3c2b823e7dff4766dcf453d895d1f1ec
venkat <venkymec1922@gmail.com>
2022-09-20 17:15:56 +0530

    payment

--- frontend.sh
+++ frontend.sh
@@ -23,6 +23,10 @@
  mv frontend-main/static/* . &>>$LOG_FILE
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 
+ echo "update Roboshop config file"
+ sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+ StatusCheck $?
+
  echo starting Nginx service
  systemctl enable nginx &>>$LOG_FILE
  systemctl restart nginx &>>$LOG_FILE
